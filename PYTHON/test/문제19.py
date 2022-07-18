710c355075b30ad2a0f496428234f54e28708fe6
ccmmaa1030 <ccmmaa1030@gmail.com>
2022-07-18 15:24:28 +0900

    TIL

--- PYTHON/test/문제19.py
+++ PYTHON/test/문제19.py
@@ -1,11 +1,18 @@
 # 양의 정수 number가 주어질 때, 숫자의 길이를 구하시오. 
 # 양의 정수number를 문자열로 변경하는 것은 금지입니다. str() 사용 금지
 
-number = input()
+number = int(input())
+n = 0
+cnt = {}
+if number > 0:
+    while True:
+        cnt[n] = number % 10
+        number = number // 10
+        n += 1
+        if number == 0:
+            break
+    count = len(cnt)
+else:
+    count = 1
 
-# cnt = 0
-# for c in number:
-#     cnt += 1
-cnt = len(number)
-
-print(cnt)
+print(count)
