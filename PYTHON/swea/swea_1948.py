d1f2485492228bce473ef38004b825c546382bf7
ccmmaa1030 <ccmmaa1030@gmail.com>
2022-11-09 23:59:56 +0900

    1109_TIL

--- PYTHON/swea/swea_1948.py
+++ PYTHON/swea/swea_1948.py
@@ -2,19 +2,19 @@ import sys
 
 sys.stdin = open("PYTHON/swea/1948_input.txt", "r")
 
-T = int(input())
+T = int(input())  # 테스트케이스 T
 
-day = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+day = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  # 달 별 일수
 
 for test_case in range(1, T + 1):
-    answer = 0
-    m1, d1, m2, d2 = map(int, input().split())
+    answer = 0  # 날짜 차이
+    m1, d1, m2, d2 = map(int, input().split())  # 날짜 2개
 
-    if m1 == m2:
-        answer = d2 - d1 + 1
-    else:
-        answer = day[m1 - 1] - d1 + 1 + d2
-        for m in range(m1, m2 - 1):
-            answer += day[m]
+    if m1 == m2:  # 같은 달이면
+        answer = d2 - d1 + 1  # 뒤 - 앞 + 1
+    else:  # 다른 달이면
+        answer = day[m1 - 1] - d1 + 1 + d2  # 앞의 달 일수차 + 뒤의 달 일수
+        for m in range(m1, m2 - 1):  # 중간 달 일수 계산
+            answer += day[m]  # 중간 일수 더하기
 
     print(f"#{test_case} {answer}")
